76070f1543a7fe480d7177cfb7f1b3ac6d407f09
Nayomi Morita <nayomi.morita@gmail.com>
2019-02-01 10:12:34 -0800

    add js layout mostly finished

--- js/scripts.js
+++ js/scripts.js
@@ -5,18 +5,75 @@ function Order() {
 }
 
 Order.prototype.addOrder = function(order) {
-  oredr.id = this.orderId()
-  this.orders.push(order)
+  order.id = this.orderId();
+  this.orders.push(order);
 }
 
+Order.prototype.assignId = function() {
+  this.orderId += 1;
+  return this.orderId;
+}
 
+Order.prototype.findOrder = function (id) {
+  for (var i = 0; i < this.orders.length; i++) {
+    if (this.orders[i]) {
+      if(this.orders[i].id == id) {
+        return this.orders[i];
+      }
+    }
+  }
+  return false;
+}
 
-
-
-
-
-function Pizza() {
-  this.toppings = topping,
+function Pizza(name, meatTopping, vegTopping, size) {
+  this.name = name,
+  this.meatTopping = meatTopping,
+  this.vegTopping = vegTopping,
   this.size = size,
   this.price = 0,
 }
+
+var order = new Order();
+
+function displayOrder(orderDisplay) {
+  var orders = $("ul#orders");
+  var orderInfo = "",
+  orderDisplay.orders.forEach(function(order) {
+    orderInfo += "<li id=" + order.id + ">" + oredr.name + ""
+  })
+  orders.html(orderInfo)
+}
+
+function showOrder (orderId) {
+  var order = Order.findOrder(orderId);
+  $("#show-order").show();
+  $(".name").html(order.name);
+  $(".meatTopping").html(order.meatTopping);
+  $(".vegTopping").html(order.vegTopping);
+  $(".size").html(order.size);
+  $(".price").html(order.price);
+}
+
+function attachOrder() {
+  $("ul#orders").on("click", "li", function() {
+    showOrder(this.id)
+  });
+}
+
+$(document).ready(function() {
+  attachOrder();
+  $("form#newPizza").submit(function(event){
+    event.preventDefault();
+    var inputtedName = $("input#newName").val();
+    var inputtedMeatTopping = $("input#newMeatTopping").val();
+    var inputtedVegTopping = $("input#newVegTopping").val();
+    var inputtedSize = $("input#size").val();
+    $("input#newName").val("");
+    $("input#newMeatTopping").val("");
+    $("input#newVegTopping").val("");
+    $("input#size").val("");
+    var newPizza = new Pizza(inputtedName, inputtedMeatTopping, inputtedVegTopping, inputtedSize);
+    order.addOrder(newPizza);
+    displayOrder(order);
+  })
+})
