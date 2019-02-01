525f70fd0af44928d61f729a93dbe003367750db
Nayomi Morita <nayomi.morita@gmail.com>
2019-02-01 08:33:03 -0800

    add constructors in js

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+
+function Order() {
+  this.orders = [],
+  this.orderId = 0,
+}
+
+Order.prototype.addOrder = function(order) {
+  oredr.id = this.orderId()
+  this.orders.push(order)
+}
+
+
+
+
+
+
+
+function Pizza() {
+  this.toppings = topping,
+  this.size = size,
+  this.price = 0,
+}
